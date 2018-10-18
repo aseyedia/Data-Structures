f70f25cdb430e767772efeeafa1f6575982ee1d0
arta <arta.seyedian@gmail.com>
2018-10-17 22:32:29 -0400

    main

--- Lab 2/src/Main.java
+++ Lab 2/src/Main.java
@@ -1,10 +1,26 @@
+import java.io.*;
+import java.util.Scanner;
 
 public class Main
     {
 
-        public static void main(String[] args)
+        public static void main(String[] args) throws FileNotFoundException
             {
-                // TODO Auto-generated method stub
+                System.out.println(
+                        "Please enter the full file name of the input.");
+                Scanner input = new Scanner(System.in);
+                String fileName = input.nextLine();
+                try
+                    {
+                        BufferedReader file =
+                                new BufferedReader(new FileReader(fileName));
+                    } catch (FileNotFoundException e)
+                    {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                        System.err.println(e.getMessage()
+                                + ": couldn't find that file, friend.");
+                    }
 
             }
 
