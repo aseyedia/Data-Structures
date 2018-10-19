9d934ac647ea7c8dc7bd561e64d3b8b328e12152
arta <arta.seyedian@gmail.com>
2018-10-19 16:04:57 -0400

    can read file now
    the program can read the file, all I have to do is make it detect when there is a new line/dimension, and make sure that it is correctly storing the vertices and their corresponding values.

--- Lab 2/src/Main.java
+++ Lab 2/src/Main.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Main
@@ -10,11 +12,12 @@ public class Main
                 System.out.println(
                         "Please enter the full file name of the input.");
                 Scanner input = new Scanner(System.in);
-                String fileName = input.nextLine();
+                String fileName = "PathsGraphInput.txt";
                 try
                     {
+                        @SuppressWarnings("resource")
                         FileReader file = new FileReader(fileName);
-                        while (file.ready() && file.read() != -1)
+                        while (file.ready())
                             {
 
                                 // Read header number of the matrix, which is
@@ -25,13 +28,49 @@ public class Main
                                 // matrix will define how many possible
                                 // direct connections the vertex in question
                                 // can have
+                                char dims = (char) file.read();
 
-                                int dims = file.read();
                                 for (int i = 0; i < dims; i++)
+                                // Maybe create an array of 1's and 0's for
+                                // each vertex
                                     {
-                                        for (int q = 0; i < dims; i++)
+                                        // This array holds the vertices that
+                                        // are in
+                                        // each adjacency graph
+                                        Vertex[] verts = new Vertex[dims];
+                                        // The vert object is created with i
+                                        // as its ID
+                                        // and dims as the max number of
+                                        // connections it
+                                        // can have
+                                        Vertex vert = new Vertex(i, dims);
+                                        // The array then stores the vertex
+                                        // in the index of its ID (or
+                                        // something)
+                                        verts[i] = vert;
+
+                                        // This nested for-loop is row-major
+                                        // order,
+                                        // so for each line, the data is going
+                                        // to be
+                                        // read into the vertex created above
+                                        // in the
+                                        // below for loop, and set
+                                        for (int q = 0; q < dims; q++)
                                             {
-                                                
+                                                char connect =
+                                                        (char) file.read();
+                                                if (connect == '0'
+                                                        || connect == '1')
+                                                    {
+                                                        vert.setEdges(connect,
+                                                                i);
+                                                    }
+                                                else if (connect == (char)-1) {
+                                                    System.out.println("Reached end of file.");
+                                                    System.exit(0);
+                                                }
+                                                System.out.print(connect);
                                             }
                                     }
                             }
--- Lab 2/src/Vertex.java
+++ Lab 2/src/Vertex.java
@@ -1,15 +1,55 @@
 public class Vertex
     {
         int edges;
+        char vert;
+        int[] conn;
+        int id;
+
+        public Vertex(int id, int dims)
+            {
+                super();
+                this.id = id;
+                this.conn = new int[dims];
+            }
+
+        public int getId()
+            {
+                return id;
+            }
+
+        public void setId(int id)
+            {
+                this.id = id;
+            }
+
+        public int[] getConn()
+            {
+                return conn;
+            }
+
+        public void setConn(int dims)
+            {
+                this.conn = new int[dims];
+            }
+
+        public char getVert()
+            {
+                return vert;
+            }
+
+        public void setVert(char vert)
+            {
+                this.vert = vert;
+            }
 
         public int getEdges()
             {
                 return edges;
             }
 
-        public void setEdges(int edges)
+        public void setEdges(char edges, int i)
             {
-                this.edges = edges;
+                conn[i] = edges;
             }
         
     }
