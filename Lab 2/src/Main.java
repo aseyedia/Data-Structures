41d20b32c765a2eeb09a5c22a2a84724a3e5f7dc
Arta Seyedian <arta.seyedian@gmail.com>
2018-10-19 09:27:18 -0400

    workin'

--- Lab 2/src/Main.java
+++ Lab 2/src/Main.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 public class Main
     {
 
-        public static void main(String[] args) throws FileNotFoundException
+        public static void main(String[] args)
+                throws IOException, FileNotFoundException
             {
                 System.out.println(
                         "Please enter the full file name of the input.");
@@ -12,9 +13,32 @@ public class Main
                 String fileName = input.nextLine();
                 try
                     {
-                        BufferedReader file =
-                                new BufferedReader(new FileReader(fileName));
-                    } catch (FileNotFoundException e)
+                        FileReader file = new FileReader(fileName);
+                        while (file.ready() && file.read() != -1)
+                            {
+
+                                // Read header number of the matrix, which is
+                                // the dimensions of the matrix, and then
+                                // process the the 1s and 0s or something
+
+                                // so I guess here the dimensions of the
+                                // matrix will define how many possible
+                                // direct connections the vertex in question
+                                // can have
+
+                                int dims = file.read();
+                                for (int i = 0; i < dims; i++)
+                                    {
+                                        for (int q = 0; i < dims; i++)
+                                            {
+                                                
+                                            }
+                                    }
+                            }
+
+                    }
+
+                catch (FileNotFoundException e)
                     {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
---
+++ Lab 2/src/Vertex.java
@@ -0,0 +1,15 @@
+public class Vertex
+    {
+        int edges;
+
+        public int getEdges()
+            {
+                return edges;
+            }
+
+        public void setEdges(int edges)
+            {
+                this.edges = edges;
+            }
+        
+    }
