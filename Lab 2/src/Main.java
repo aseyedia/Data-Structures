3e71ede388eeb69bd350e0d9ea236ef1f00d28a9
arta <arta.seyedian@gmail.com>
2018-10-20 16:19:18 -0400

    got the thing to work

--- Lab 2/src/Main.java
+++ Lab 2/src/Main.java
@@ -9,85 +9,18 @@ public class Main {
 	String fileName = "PathsGraphInput.txt";
 	try {
 	    @SuppressWarnings("resource")
-	    FileReader file = new FileReader(fileName);
+	    MatrixRead file = new MatrixRead(fileName);
 	    while (file.ready()) {
 
-		// Read header number of the matrix, which is
-		// the dimensions of the matrix, and then
-		// process the the 1s and 0s or something
+		// Spent way too much time like an idiot trying to implement this thing
+		// in a way that was much harder than it needed to be.
 
-		// so I guess here the dimensions of the
-		// matrix will define how many possible
-		// direct connections the vertex in question
-		// can have
-		int dims = Character.getNumericValue((char) file.read());
-		file.read();
+		file.readMatrix();
+
 		// in the case of a two digit dimension - THIS IS kind of fucked up, take care
-		// of it later
-		// char next = (char) file.read();
-		// if (boolean k = ) {
-		// // sometimes java is weird and the converse of this
-		// // if statement doesn't work
-		// } else {
-		// dims = dims * 10 + Character.getNumericValue(next);
-		// }
 
-		// This array holds the vertices that
-		// are in
-		// each adjacency graph
-		Vertex[] verts = new Vertex[dims];
-		// Have to line break so the dimension line
-		// doesn't get counted as a row
-		for (int row = 0; row < dims; row++)
-		// Maybe create an array of 1's and 0's for
-		// each vertex
-		{
-
-		    // The vert object is created with i
-		    // as its ID
-		    // and dims as the max number of
-		    // connections it
-		    // can have
-		    Vertex vert = new Vertex(row, dims);
-
-		    // This nested for-loop is row-major
-		    // order,
-		    // so for each line, the data is going
-		    // to be
-		    // read into the vertex created above
-		    // in the
-		    // below for loop, and set
-		    for (int col = 0; col < 2 * dims + 2; col++)
-		    // ERROR this is incrementing for whitespaces and linebreaks
-		    {
-			char connect = (char) file.read();
-			if (connect == '0' || connect == '1') {
-			    vert.setEdges(Character.getNumericValue(connect), col / 2);
-			    // The array then stores
-			    // the vertex
-			    // in the index of its
-			    // ID (or
-			    // something)
-			    verts[row] = vert;
-			} else if (connect == (char) -1) {
-			    System.out.println("Reached end of file.");
-			    System.exit(0);
-
-			} else if (connect == ' ') {
-			    // continue;
-			} else if (connect == '\n') {
-			    // System.out.println();
-			    break;
-			    
-			}
-			System.out.print(Character.toString(connect));
-		    }
-		}
 	    }
-
-	}
-
-	catch (FileNotFoundException e) {
+	} catch (FileNotFoundException e) {
 	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	    System.err.println(e.getMessage() + ": couldn't find that file, friend.");
---
+++ Lab 2/src/MatrixRead.java
@@ -0,0 +1,95 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * After spending far too long trying to figure out how to properly read in the
+ * matrix text file using the conditions outlined in the office hours recording,
+ * I decided to just look for an example of somebody online reading a text
+ * matrix and adapt it to my needs.
+ * 
+ * @author Arta
+ *
+ */
+public class MatrixRead {
+
+    String fileName;
+    Scanner sc;
+
+    public MatrixRead(String fileName) throws FileNotFoundException {
+	// TODO Auto-generated constructor stub
+	this.fileName = fileName;
+	this.sc = new Scanner(new BufferedReader(new FileReader(fileName)));
+    }
+
+    public MatrixRead() {
+	// TODO Auto-generated constructor stub
+    }
+
+    /**
+     * This method was borrowed in large part from an online source:
+     * 
+     * Sai, G. (2018, January 3). How to read a 2d array from a file in java.
+     * Retrieved October 20, 2018, from
+     * https://www.tutorialspoint.com/How-to-read-a-2d-array-from-a-file-in-java
+     * 
+     * This method takes the number at the top of each matrix and sets that as the
+     * dimensions (dims). Then, it creates an array based on the dimensions, and
+     * sets the finally element to -1 as a marker of incompleteness. It then
+     * interprets the matrix. Once it is done, it looks for another dimension.
+     */
+    public void readMatrix() throws FileNotFoundException {
+	// May be redundant to have the hasNextLine function and
+	// the ready method, but it's okay
+	if (sc.hasNextLine()) {
+	    int dims = sc.nextInt();
+	    System.out.println(dims);
+	    sc.nextLine();
+	    int rows = dims;
+	    int columns = dims;
+	    int[][] myArray = new int[rows][columns];
+	    myArray[rows - 1][columns - 1] = -1;
+	    while (myArray[rows - 1][columns - 1] == -1) {
+		for (int i = 0; i < myArray.length; i++) {
+		    String[] line = sc.nextLine().trim().split(" ");
+		    // What I can do here is create a vertex for each
+		    // row, and then commit the values of the matrix
+		    // row to that vertex's connections.
+		    // Then, I can export the vertex using a separate method
+		    // that would return that vertex either to main, or,
+		    // now that I'm thinking about it, a class that stores
+		    // vertices or something. I'll figure it out. 
+		    for (int j = 0; j < line.length; j++) {
+			myArray[i][j] = Integer.parseInt(line[j]);
+		    }
+		}
+	    }
+	    System.out.println(Arrays.deepToString(myArray));
+	    
+	}
+    }
+
+    /**
+     * Generates vertices from the array, returns them to be stored
+     * in main. 
+     * 
+     * @param myArray
+     *            2-D array from the matrix which represents the graph
+     */
+    public Vertex exportVertex(int[][] myArray) {
+	return null;
+
+    }
+
+    public boolean ready() {
+	boolean ready = false;
+	if (sc.hasNextLine()) {
+	    ready = true;
+	}
+	return ready;
+    }
+
+}
